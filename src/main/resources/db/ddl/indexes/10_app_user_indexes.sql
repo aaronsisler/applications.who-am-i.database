507d5671016ec8abfafa755b2d357f8bd0d19869
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2026-02-14 11:39:47 -0500

    Updating the app user table have a different id structure

--- src/main/resources/db/ddl/indexes/10_app_user_indexes.sql
+++ src/main/resources/db/ddl/indexes/10_app_user_indexes.sql
@@ -1,11 +1,7 @@
--- 1. Support logins / identity lookups
-CREATE INDEX IF NOT EXISTS idx_app_user_email
-    ON auth.app_user (email_address);
-
--- 2. Useful for ordering and retrieving recently created users
+-- 1. Useful for ordering and retrieving recently created users
 CREATE INDEX IF NOT EXISTS idx_app_user_created_at
     ON auth.app_user (created_at);
 
--- 3. Useful for filtering or searching by last name
+-- 2. Useful for filtering or searching by last name
 CREATE INDEX IF NOT EXISTS idx_app_user_last_name
     ON auth.app_user (last_name);
--- src/main/resources/db/ddl/tables/10_app_user_table.sql
+++ src/main/resources/db/ddl/tables/10_app_user_table.sql
@@ -2,8 +2,8 @@
 * A user of the application
 */
 CREATE TABLE IF NOT EXISTS auth.app_user (
-  id bigint primary key generated always as identity,
-  user_id uuid NOT NULL,
+  internal_id bigint primary key generated always as identity,
+  external_id uuid NOT NULL,
   email_address varchar(100) NOT NULL,
   first_name varchar(45) NOT NULL,
   last_name varchar(45) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS auth.app_user (
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-  CONSTRAINT uq_app_user_user_id UNIQUE (user_id),
+  CONSTRAINT uq_app_user_external_id UNIQUE (external_id),
   CONSTRAINT uq_app_user_email UNIQUE (email_address),
   CONSTRAINT chk_email_not_empty CHECK (email_address <> ''),
   CONSTRAINT chk_first_name_not_empty CHECK (first_name <> ''),
